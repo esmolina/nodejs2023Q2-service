2b92660086d7f6283b0b2f38151554c66a427189
esmolina <elenasmolinann@gmail.com>
2023-07-31 08:51:03 +0300

    feat: implement getting port number from dotenv

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { config } from 'dotenv';
+
+const env = config();
+const PORT = Number(env.parsed.PORT) || 3000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(4000);
+  await app.listen(PORT);
 }
+
 bootstrap();
