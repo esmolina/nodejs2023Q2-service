13b97590ee9a4a29240688e9217b6af1f6b5ee8d
esmolina <elenasmolinann@gmail.com>
2023-07-31 10:25:59 +0300

    refactor: fix eslint errors

--- src/user/helpers/getUserWithoutPassword.ts
+++ src/user/helpers/getUserWithoutPassword.ts
@@ -1,5 +1,4 @@
 import { UserInterface } from '../interfaces/user-interface';
-import { GetUserDto } from '../dto/get-user.dto';
 import { UserEntity } from '../entities/user.entity';
 
 export const getUserWithoutPassword = (user: UserInterface): UserEntity => {
